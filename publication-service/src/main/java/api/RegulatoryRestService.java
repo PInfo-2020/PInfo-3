6557c914adb63e31f1d6587dbf28f9d1c053e777
elhamAm <elham.aminmansour@gmail.com>
2020-03-17 11:15:25 +0100

    creates the right folders, still need to fix the pom files and dockers

---
+++ publication-service/src/main/java/api/RegulatoryRestService.java
@@ -0,0 +1,13 @@
+package api;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Path;
+
+@ApplicationScoped
+@Path("/regulatory")
+public class RegulatoryRestService {
+
+
+
+
+}
