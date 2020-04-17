4c6aebd34be04f3d4db44b8df1f8429f6659487d
Luka <luka.lukic@etu.unige.ch>
2020-04-17 21:48:33 +0200

    pom

--- recipes-service/src/test/java/domain/service/RecipeServiceTest.java
+++ recipes-service/src/test/java/domain/service/RecipeServiceTest.java
@@ -1,56 +1,56 @@
-package domain.service;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.List;
-import java.util.UUID;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import domain.model.Recipe;
-import eu.drus.jpa.unit.api.JpaUnit;
-
-@ExtendWith(JpaUnit.class)
-@ExtendWith(MockitoExtension.class)
-public class RecipeServiceTest {
-	@Spy
-	@PersistenceContext(unitName = "RecipePUTest")
-	EntityManager em;
-
-	@InjectMocks
-	private RecipeServiceImpl recipeServiceImpl;
-
-//	@Test
-//	void testGetAll() {
-//		List<Recipe> recipes = recipeServiceImpl.getAll();
-//		int size = recipes.size();
-//		
-//		recipeServiceImpl.create(getRandomRecipe());
-//		recipeServiceImpl.create(getRandomRecipe());
-//		recipeServiceImpl.create(getRandomRecipe());
-//		recipeServiceImpl.create(getRandomRecipe());
-//		
-//		assertEquals(size + 4, recipeServiceImpl.getAll().size());
-//	}
-//	
-//	@Test
-//	void testCreate() {
-//		Recipe recipe = getRandomRecipe();
-//		recipeServiceImpl.create(recipe);
-//		assertNotNull(recipe.getId());
-//	}
-//	
-//	private Recipe getRandomRecipe() {
-//		Recipe recipe = new Recipe(UUID.randomUUID().toString());
-//		return recipe;
-//	}
-
-}
+//package domain.service;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Spy;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import domain.model.Recipe;
+//import eu.drus.jpa.unit.api.JpaUnit;
+//
+//@ExtendWith(JpaUnit.class)
+//@ExtendWith(MockitoExtension.class)
+//public class RecipeServiceTest {
+//	@Spy
+//	@PersistenceContext(unitName = "RecipePUTest")
+//	EntityManager em;
+//
+//	@InjectMocks
+//	private RecipeServiceImpl recipeServiceImpl;
+//
+////	@Test
+////	void testGetAll() {
+////		List<Recipe> recipes = recipeServiceImpl.getAll();
+////		int size = recipes.size();
+////		
+////		recipeServiceImpl.create(getRandomRecipe());
+////		recipeServiceImpl.create(getRandomRecipe());
+////		recipeServiceImpl.create(getRandomRecipe());
+////		recipeServiceImpl.create(getRandomRecipe());
+////		
+////		assertEquals(size + 4, recipeServiceImpl.getAll().size());
+////	}
+////	
+////	@Test
+////	void testCreate() {
+////		Recipe recipe = getRandomRecipe();
+////		recipeServiceImpl.create(recipe);
+////		assertNotNull(recipe.getId());
+////	}
+////	
+////	private Recipe getRandomRecipe() {
+////		Recipe recipe = new Recipe(UUID.randomUUID().toString());
+////		return recipe;
+////	}
+//
+//}
