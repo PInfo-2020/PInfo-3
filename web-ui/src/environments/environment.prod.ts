9219f9b2d2fd8374ed38f4fac2b1e8aef0d86ea2
RoxaneBurri <roxane.burri@etu.unige.ch>
2020-04-21 16:51:55 +0200

    keycloak and path config

--- web-ui/src/environments/environment.prod.ts
+++ web-ui/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
-export const environment = {
-  production: true
-};
+import { DynamicEnvironment } from './dynamic-environment';
+class Environment extends DynamicEnvironment {
+  public production: boolean;
+  constructor() {
+    super();
+  }
+}
+
+export const environment = new Environment();
