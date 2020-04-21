cc49b6ca537d61763b0e6e29a93ab03c3d50337b
elhamAm <elham.aminmansour@gmail.com>
2020-04-21 18:49:10 +0200

    fixing apigateway

--- api-gateway/src/main/docker/docker-entrypoint.sh
+++ api-gateway/src/main/docker/docker-entrypoint.sh
@@ -3,9 +3,14 @@ set -e
 
 echo "here2"
 
+
 export KONG_NGINX_DAEMON=off
-#chmod 777 /usr/local/kong/ssl/certs
-#chown -R kong /usr/local/kong/ssl/certs
+#ls -lr /usr/local/kong/ssl
+#whoami
+#chmod -R 777 /usr/local
+#echo "done"
+#whoami
+#chmod -R 777 /usr/local/kong
 
 echo "here1"
 
@@ -13,15 +18,24 @@ has_transparent() {
   echo "$1" | grep -E "[^\s,]+\s+transparent\b" >/dev/null
 }
 
+#chmod 777 /usr/
+#chmod 777 /usr/local/kong/ssl/certs
+#chown -R kong /usr/
 if [[ "$1" == "kong" ]]; then
   PREFIX=${KONG_PREFIX:=/usr/local/kong}
 
   if [[ "$2" == "docker-start" ]]; then
     shift 2
     echo "chaning permission"
+    whoami
+    #chmod -R a+rwx /usr/local/kong
     kong prepare -p "$PREFIX" "$@"
+    #chmod -R 777 /usr/local/kong/ssl/certs   
+    echo "before"
     chown -R kong "$PREFIX"
-
+    echo "after"
+    #chmod -R 777 /usr/local/kong/ssl/certs
+    #chown -R kong /usr/local/kong/ssl/certs
     # workaround for https://github.com/moby/moby/issues/31243
     chmod o+w /proc/self/fd/1
     chmod o+w /proc/self/fd/2
