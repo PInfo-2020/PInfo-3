dad2672dd97e1e0db319bb863be21666c9a1f0a3
Nathan <natco95@hotmail.com>
2020-05-08 17:28:50 +0200

    back to another commit

--- lists-service/src/main/resources/META-INF/test_data.sql
+++ lists-service/src/main/resources/META-INF/test_data.sql
@@ -1,5 +1,4 @@
 INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES (1, 1, 1);
-INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES (1, 2, 2);
 INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES (2, 2, 2);
 INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES (3, 3, 3);
 INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES (1, 1, 1);
