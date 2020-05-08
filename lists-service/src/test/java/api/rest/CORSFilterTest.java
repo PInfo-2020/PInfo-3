59717873f84c5eb11968574c0699a11e50c14267
Nathan <natco95@hotmail.com>
2020-05-08 15:35:25 +0200

    small bug corrections

--- lists-service/src/test/java/api/rest/CORSFilterTest.java
+++ lists-service/src/test/java/api/rest/CORSFilterTest.java
@@ -1,4 +1,4 @@
-package api;
+package api.rest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
--- lists-service/src/test/java/api/rest/ListsRestServiceIT.java
+++ lists-service/src/test/java/api/rest/ListsRestServiceIT.java
@@ -1,4 +1,4 @@
-package api;
+package api.rest;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
