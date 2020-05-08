1c518e1b9c3dfaa175ce427c2b74fc5667bb5ec6
Luka <luka.lukic@etu.unige.ch>
2020-05-08 23:10:45 +0200

    kafka pls 2

--- recipes-service/src/main/java/api/rest/RecipeRestService.java
+++ recipes-service/src/main/java/api/rest/RecipeRestService.java
@@ -167,7 +167,7 @@ public class RecipeRestService {
 		
 		while(!test) {
 			try {
-				Thread.sleep(10000);
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
