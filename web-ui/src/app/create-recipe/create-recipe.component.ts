15741c2e426614356a883b6e373162b932fcc172
Nathan <natco95@hotmail.com>
2020-05-15 16:47:16 +0200

    else

--- web-ui/src/app/create-recipe/create-recipe.component.ts
+++ web-ui/src/app/create-recipe/create-recipe.component.ts
@@ -9,7 +9,9 @@ import {KeycloakInstance} from 'keycloak-js';
 })
 export class CreateRecipeComponent implements OnInit {
   public keycloakAuth: KeycloakInstance;
-  constructor(public keycloak: KeycloakService){}
+  constructor(public keycloak: KeycloakService){
+    
+   }
 
   ngOnInit(): void {
     this,this.keycloakAuth = this.keycloak.getKeycloakAuth();
@@ -19,36 +21,36 @@ export class CreateRecipeComponent implements OnInit {
   }
 
   plus(){
-    // let quantity = document.getElementById("quantity");
-    // quantity.value++;
+    let quantity = document.getElementById("quantity");
+    quantity.value++;
     }
 
   minus(){
-    // let quantity = document.getElementById("quantity");
-    // if (quantity.value > 0) {
-    //   quantity.value--;
-    // }
+    let quantity = document.getElementById("quantity");
+    if (quantity.value > 0) {
+      quantity.value--;
+    }
   }
 
   addBlock1() {
-    // let ingredient = document.getElementById("ingredient").value;
-    // let quantity = document.getElementById("quantity").value;
-    //
-    // if (ingredient && quantity) {
-    //   let idIng = "ingredient-".concat(ingredient);
-    //   if (document.getElementById(idIng)) {
-    //     let idQuan = ingredient.concat("-quantity");
-    //     let textnode = document.createTextNode(quantity);
-    //     idQuan.textContent = quantity;
-    //   }
-    //   else {
-    //     let blockToAdd = document.createElement("div");
-    //     blockToAdd.className = "row mb-1";
-    //     blockToAdd.innerHTML = "<div class=\"col border w-50 text-center\" id=\"ingredient-" + ingredient + "\">" + ingredient + "</div><div class=\"col border w-50 text-center\" id=" + ingredient + "-quantity" + "\">" + quantity + "</div>";
-    //
-    //     let blockContainer = document.getElementById("div1");
-    //     blockContainer.appendChild(blockToAdd);
-    //   }
-    // }
+    let ingredient = document.getElementById("ingredient").value;
+    let quantity = document.getElementById("quantity").value;
+
+    if (ingredient && quantity) {
+      let idIng = "ingredient-".concat(ingredient);
+      if (document.getElementById(idIng)) {
+        let idQuan = ingredient.concat("-quantity");
+        let textnode = document.createTextNode(quantity);
+        idQuan.textContent = quantity;
+      }
+      else {
+        let blockToAdd = document.createElement("div");
+        blockToAdd.className = "row mb-1";
+        blockToAdd.innerHTML = "<div class=\"col border w-50 text-center\" id=\"ingredient-" + ingredient + "\">" + ingredient + "</div><div class=\"col border w-50 text-center\" id=" + ingredient + "-quantity" + "\">" + quantity + "</div>";
+
+        let blockContainer = document.getElementById("div1");
+        blockContainer.appendChild(blockToAdd);
+      }
+    }
   }
 }
