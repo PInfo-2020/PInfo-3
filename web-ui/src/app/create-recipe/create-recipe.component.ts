efe1427d651d9d1568d94483b0adaaffec4660f0
Nathan <natco95@hotmail.com>
2020-05-18 14:52:20 +0200

    Adding features and communication backend frontend

--- web-ui/src/app/create-recipe/create-recipe.component.ts
+++ web-ui/src/app/create-recipe/create-recipe.component.ts
@@ -1,80 +1,113 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { KeycloakService } from '../services/keycloak/keycloak.service';
 import { KeycloakInstance } from 'keycloak-js';
 import * as $ from 'jquery';
-import { Ingredient } from './ingredient.component';
+import { IngredientService } from './ingredientService';
+import { Ingredient } from './ingredient';
 
 @Component({
   selector: 'app-create-recipe',
   templateUrl: './create-recipe.component.html',
   styleUrls: ['./create-recipe.component.css'],
 })
-export class CreateRecipeComponent implements OnInit {
-  public keycloakAuth: KeycloakInstance;
-  constructor(public keycloak: KeycloakService){
+export class CreateRecipeComponent implements OnInit, AfterViewInit {
+  ingredientsDB: Array<Ingredient> = [];
+  counterInstruction: number = 1;
+  quantityElem: any;
+  ingredientElem: any;
+  instructionElem: any;
 
-  }
+  public keycloakAuth: KeycloakInstance;
+
+  constructor(public keycloak: KeycloakService, public ingredientService: IngredientService){}
 
   ngOnInit(): void {
-    this,this.keycloakAuth = this.keycloak.getKeycloakAuth();
-    if(this.keycloak.isLoggedIn() == false){
-      this.keycloak.login();
+    this.keycloakAuth = this.keycloak.getKeycloakAuth();
+    if (this.keycloak.isLoggedIn() === false) {
+        this.keycloak.login();
     }
+  }
+
+  ngAfterViewInit() {
+    this.quantityElem = document.getElementById("quantity");
+    this.ingredientElem = document.getElementById("ingredient");
+    this.instructionElem = document.getElementById("instruction");
+
+    let ingredientDataElem = document.getElementById("ingredients-choices");
+
+    this.ingredientService.getIngredients()
+      .subscribe((data: Ingredient[]) => {
+        this.ingredientsDB = data;
+
+        this.ingredientsDB.forEach(function(item){
+          var option = document.createElement("option");
+          option.value = item.name;
+          ingredientDataElem.appendChild(option);
+        });
+      });
   }
 
   plus(){
-    let quantity: any = document.getElementById("quantity");
-    quantity.value++;
+    this.quantityElem.value++;
   }
 
   minus(){
-    let quantity: any = document.getElementById("quantity");
-    if (quantity.value > 0) {
-      quantity.value--;
+    if (this.quantityElem.value > 0) {
+      this.quantityElem.value--;
     }
   }
 
-  ingredients: Array<Ingredient> = [];
   addBlock1() {
-    let ingredient: any = (document.getElementById("ingredient") as HTMLInputElement).value;
-    let quantity: any = (document.getElementById("quantity") as HTMLInputElement).value;
+    let ingredientVal = this.ingredientElem.value;
+    let quantityVal = this.quantityElem.value;
+    let ingredientsName = []
+    this.ingredientsDB.forEach(function(item){
+      ingredientsName.push(item.name);
+    });
 
-    if (ingredient && quantity) {
-      let idIng = "ingredient-".concat(ingredient);
+    if (ingredientsName.includes(ingredientVal) && (quantityVal>0)) {
+      let idIng = "ingredient-".concat(ingredientVal);
       if (!document.getElementById(idIng)) {
-        let idQuantity = ingredient.concat("-quantity");
         let blockToAdd = document.createElement("div");
-        let idRow = ingredient.concat("-row");
         blockToAdd.className = "row mb-1 text-center";
-        blockToAdd.id = idRow;
         blockToAdd.innerHTML = `
-          Nom:<div class="col-4 border m-auto" id= ${idIng}>${ingredient}</div>
-          Quantité:<div class="col-4 border m-auto" id= ${idQuantity}>${quantity}</div>
-          <!-- <button type="button" class="btn btn-secondary mr-1 button-w" onclick="(function(){let id: any = document.getElementById("poulet-row"); id.remove();})()">X</button> -->
+          <span class="m-auto">Name:</span><span class="col-4 border m-auto" id= ${idIng}>${ingredientVal}</span>
+          <span class="m-auto">Quantity:</span><span class="col-4 border m-auto">${quantityVal}</span>
+          <button type="button" class="btn btn-secondary mr-1 button-w" onclick="this.parentNode.remove();">x</button>
           `;
 
         let blockContainer = document.getElementById("div1");
         blockContainer.appendChild(blockToAdd);
-        this.ingredients.push(new Ingredient(ingredient, quantity))
-        console.log(this.ingredients);
       }
     }
   }
 
-  counter: number = 1;
   addBlock2() {
-    let description: any = (document.getElementById("descriptionInstruction") as HTMLInputElement).value;
+    let instructionVal = this.instructionElem.value;
 
-    if (description) {
+    if (instructionVal) {
       let blockToAdd = document.createElement("div");
       blockToAdd.className = "row mb-1 text-center";
       blockToAdd.innerHTML = `
-        <span class="m-auto">${this.counter}.</span><div class="col-8 border m-auto border">${description}</div>
+        <span class="m-auto">${this.counterInstruction}.</span><div class="col-8 border m-auto border">${instructionVal}</div>
+        <button type="button" class="btn btn-secondary mr-1 button-w" onclick="this.parentNode.remove();">x</button>
       `;
 
       let blockContainer = document.getElementById("div2");
       blockContainer.appendChild(blockToAdd);
-      this.counter++;
+      this.counterInstruction++;
+    }
+  }
+
+  handleKeyPress(e) {
+    var code = (e.which) ? e.which : e.keyCode;
+    let quantityVal = this.quantityElem.value.split('');
+    let countDot = quantityVal.filter((v) => (v === '.')).length;
+    if (code == 46 && countDot == 0){
+      return true;
+    }
+    if (code > 31 && (code < 48 || code > 57)) {
+        e.preventDefault();
     }
   }
 }
--- web-ui/src/app/create-recipe/ingredient.component.ts
+++
@@ -1,9 +0,0 @@
-export class Ingredient{
-  private Ingredient: string;
-  private Quantity: number;
-
-  constructor(Ingredient, Quantity) {
-      this.Ingredient = Ingredient;
-      this.Quantity = Quantity;
-  }
-}
