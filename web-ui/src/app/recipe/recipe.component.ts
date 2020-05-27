1aa68bc5b76538299f3ee42d5e14f24c3f5bb6f5
WatiBenj <benjamin.fischer@etu.unige.ch>
2020-05-27 21:13:21 +0200

    Nathan is a pussymagnet

--- web-ui/src/app/recipe/recipe.component.ts
+++ web-ui/src/app/recipe/recipe.component.ts
@@ -1,15 +1,118 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { KeycloakService } from '../services/keycloak/keycloak.service';
+import { KeycloakInstance } from 'keycloak-js';
+import { RecipeService } from './recipeService';
+import { Recipe } from './recipe';
+import { IngredientService } from './ingredientService';
+import { Ingredient } from './ingredient';
+import { IngredientRecipe } from './ingredientRecipe';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
-export class RecipeComponent implements OnInit {
+export class RecipeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  recipeDB: Recipe;
+  ingredientsDB: Array<String> = [];
+  ingredientsRecipeDB: Array<IngredientRecipe> = [];
+  counterInstruction: number = 1;
+  nameElem: any;
+  peopleElem: any;
+  timeElem: any;
+  gradeElem: any;
+  newGradeElem: any;
+  descriptionElem: any;
+  ingredientElem: any;
+  quantityElem: any;
+  instructionElem: any;
+  commentElem: any;
+
+  public keycloakAuth: KeycloakInstance;
+  constructor(public keycloak: KeycloakService, private recipeService: RecipeService, private ingredientService: IngredientService, private router: Router) { }
 
   ngOnInit(): void {
+    // this.keycloakAuth = this.keycloak.getKeycloakAuth();
+    // if (this.keycloak.isLoggedIn() == false) {
+    //   this.keycloak.login();
+    // }
+  }
+
+  ngAfterViewInit() {
+
+    this.nameElem = document.getElementById("name");
+    this.peopleElem = document.getElementById("people");
+    this.timeElem = document.getElementById("time");
+    this.gradeElem = document.getElementById("grade");
+    this.newGradeElem = document.getElementById("new_grade");
+    this.descriptionElem = document.getElementById("description");
+    this.ingredientElem = document.getElementById("ingredient");
+    this.quantityElem = document.getElementById("quantity");
+    this.instructionElem = document.getElementById("instruction");
+
+    let url = this.router.url;
+    let split = url.split("recipe/");
+    let id = Number(split[1]);
+    let that = this;
+
+    this.recipeService.getRecipe(id)
+      .subscribe((data: Recipe) => {
+        that.recipeDB = data;
+        console.log(id);
+      	that.call1(id);
+      	that.call2();
+      	that.call3();
+      });
+
+  }
+
+  call1(id) {
+    let that = this;
+    console.log(id);
+    this.recipeService.getIngredientsRecipe(id)
+      .subscribe((data: IngredientRecipe[]) => {
+        that.ingredientsRecipeDB = data;
+      });
+  }
+
+  call2() {
+    let that = this;
+    this.ingredientsRecipeDB.forEach(function(item) {
+      that.ingredientService.getIngredient(item.ingredientId)
+        .subscribe((data: Ingredient) => {
+          that.ingredientsDB.push(data.name);
+        });
+    });
+  }
+
+  call3() {
+    let that = this;
+    console.log(that.recipeDB.name);
+    this.nameElem.value = that.recipeDB.name;
+    this.peopleElem.value = that.recipeDB.personnes;
+    this.timeElem.value = that.recipeDB.minutes;
+    this.descriptionElem.value = that.recipeDB.description;
+    // this.instructionElem.value = that.recipeDB.instructions;
+  }
+
+  add_shop_list() {
+
+
+  }
+
+  give_grade() {
+
+  }
+
+  add_comment() {
+    // this.recipeDB.comment = this.commentElem.value;
+
+  }
+
+  cook() {
+
   }
 
 }
---
+++ web-ui/src/app/recipe/recipeService.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Recipe } from './recipe';
+import { IngredientRecipe } from './ingredientRecipe'
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+       headers: new HttpHeaders({
+           'Content-Type': 'application/json',
+       }),
+   };
+
+   getRecipe(id: Number): Observable<Recipe> {
+     return this.http.get<Recipe>(environment.recipeService.url + "/" + id)
+       .pipe(
+                 retry(1),
+                 catchError(this.handleError),
+             );
+  }
+
+  getIngredientsRecipe(id: Number): Observable<IngredientRecipe[]> {
+    return this.http.get<IngredientRecipe[]>(environment.recipeService.url + "/" + id + "/ingredient")
+      .pipe(
+                retry(1),
+                catchError(this.handleError),
+            );
+  }
+
+  handleError(error) {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            // Get client-side error
+            errorMessage = error.error.message;
+        } else {
+            // Get server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+  }
+}
