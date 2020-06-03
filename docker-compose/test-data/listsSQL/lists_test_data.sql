02b3dc651d54178e4a54ef5f51e87ea45476d6c7
RoxaneBurri <roxane.burri@etu.unige.ch>
2020-06-03 23:49:03 +0200

    from cart to fridge

--- docker-compose/test-data/listsSQL/lists_test_data.sql
+++ docker-compose/test-data/listsSQL/lists_test_data.sql
@@ -10,12 +10,12 @@ CREATE TABLE ITEMFRIDGE(
 	ingredientID BIGINT not null,
 	quantity FLOAT(53) not null
 );
-INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES ('1', 1, 1);
+-- INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES ('1', 1, 1);
 INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES ('2', 2, 2);
 INSERT INTO ITEMCART (userID, ingredientID, quantity) VALUES ('3', 3, 3);
-INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('1', 1, 3);
-INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('1', 8, 5);
-INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('1', 7, 3);
+-- INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('1', 1, 3);
+-- INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('1', 8, 5);
+-- INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('1', 7, 3);
 INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('2', 2, 2);
 INSERT INTO ITEMFRIDGE (userID, ingredientID, quantity) VALUES ('3', 3, 3);
 
