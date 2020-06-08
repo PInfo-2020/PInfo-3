e0a6c2a4288566dab7375fdf042209aaf2b8a48f
elhamAm <elham.aminmansour@gmail.com>
2020-06-08 20:46:27 +0200

    corr helm charts

--- helm-charts/microservices/run.sh
+++ helm-charts/microservices/run.sh
@@ -1,18 +1,18 @@
-kubectl create secret generic keycloak-realm-secret --from-file=../../docker-compose/realm-export.json 
+microk8s.kubectl create secret generic keycloak-realm-secret --from-file=../../docker-compose/realm-export.json
 
-kubectl delete configmap lists-scripts
-kubectl create configmap lists-scripts  --from-file ../../docker-compose/test-data/listsSQL/lists_test_data.sql 
+microk8s.kubectl delete configmap lists-scripts
+microk8s.kubectl create configmap lists-scripts  --from-file ../../docker-compose/test-data/listsSQL/lists_test_data.sql
 
-kubectl delete configmap ingred-scripts
-kubectl create configmap ingred-scripts  --from-file ../../docker-compose/test-data/ingredSQL/ingredients.sql
-
-
-kubectl delete configmap profile-scripts
-kubectl create configmap profile-scripts  --from-file ../../docker-compose/test-data/profileSQL/profile_test_data.sql
+microk8s.kubectl delete configmap ingred-scripts
+microk8s.kubectl create configmap ingred-scripts  --from-file ../../docker-compose/test-data/ingredSQL/ingredients.sql
 
 
-kubectl delete configmap rec-scripts
-kubectl create configmap rec-scripts  --from-file ../../docker-compose/test-data/recipeSQL/recipes_test_data.sql
+microk8s.kubectl delete configmap profile-scripts
+microk8s.kubectl create configmap profile-scripts  --from-file ../../docker-compose/test-data/profileSQL/profile_test_data.sql
 
 
-microk8s.helm install random .
+microk8s.kubectl delete configmap rec-scripts
+microk8s.kubectl create configmap rec-scripts  --from-file ../../docker-compose/test-data/recipeSQL/recipes_test_data.sql
+
+microk8s enable helm3
+microk8s.helm3 install random .
